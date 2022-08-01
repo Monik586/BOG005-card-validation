fd90f1a6d56d847a04642eadccea30cb063086bf
Monik586 <monik.rodriguez586@gmail.com>
2022-08-01 11:38:10 -0500

    4 avance

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
-//import validator from './validator.js';
+import validator from './validator.js';
+// console.log(validator)
 
-//console.log(validator);
-function capturaDeDatos () {
-    const cardNumber=document.getElementById("cardNumber").value;
-    console.log(cardNumber);
+let btnValidar = document.getElementById("enviar");
+btnValidar.addEventListener("click", clickbtnValidar);//when"click" =>function "clickbtnValidar"
 
+
+function clickbtnValidar(event) {
+    event.preventDefault();
+    let creditCardNumber = document.getElementById("cardNumber").value;//let "creditCardNumber" to be used on validator.isValid
+    // console.log(creditCardNumber);
+
+    
+    let detalle = document.getElementById("resultado");
+    let validResult = validator.isValid(creditCardNumber);//result from validator
+    detalle.innerText = validResult;
+
+    if (validResult === true) {
+        detalle.style.color = "green";
+        detalle.innerText = "Valid Card"
+    } 
+    
+    else {
+        detalle.style.color = "red";
+        detalle.innerText = "Invalid Card"
+
+    }
+     
+     document.getElementById("cardNumber").value =  validator.maskify(creditCardNumber)
+    
    
-}
-document.getElementById("enviar").addEventListener("click", capturaDeDatos);
+    }
+
+const primeraPagina = document.getElementById("pagina1");
+const segundaPagina = document.getElementById("pagina2");
+const mostrarPagina2 = document.getElementById("mostrarPagina2");
+
+mostrarPagina2.addEventListener("click", () => {
+    primeraPagina.style.display = "none"; 
+    segundaPagina.style.display = "block";
+});
+  
+
+ 
+  
+   
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,46 @@
 const validator = {
-  // ...
-};
+
+  isValid: function (creditCardNumber) {
+
+    let digits = creditCardNumber.split("").reverse();
+  
+    let sum = 0;
+
+    //iterating "creditCardNumber" line 11 index.js.
+
+    for (let i = 0; i < creditCardNumber.length; i++) {
+      digits[i] = parseInt(digits[i]);
+     
+
+      if (i % 2 !== 0) {
+        digits[i] *= 2
+
+        if (digits[i] > 9) {
+          digits[i] -= 9
+        }
+      }
+
+      sum += digits[i];
+    }
+    // console.log(sum);
+    //Results.
+    if (sum % 10 === 0) {
+      // console.log("TRUE")
+      return true
+    } else {
+      // console.log("FALSE")
+      return false
+    }
+  },
+  maskify: function (creditCardNumber) {
+
+    let maskDigits= creditCardNumber.slice(0,-4).replace(/./g,"#");
+    let ultimosCuatro= creditCardNumber.slice(-4);
+    let todo= maskDigits + ultimosCuatro;
+
+    return todo
+}
+
+}
 
 export default validator;
